d3de0fc272c178ffebe82c08af8f43ff6aed18d5
jwbswll <boswelljackwilliam@gmail.com>
2023-10-18 16:17:50 +1100

    all basic functionality

--- todolist/todolist/ViewModels/ToDoListItemViewViewModel.swift
+++ todolist/todolist/ViewModels/ToDoListItemViewViewModel.swift
@@ -5,4 +5,28 @@
 //  Created by Jack Boswell on 18/10/23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
+
+class ToDoListItemViewViewModel: ObservableObject {
+    
+    
+    init() {}
+    
+    func toggleIsDone(item: ToDoListItem) {
+        var itemClone = item
+        itemClone.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemClone.id)
+            .setData(itemClone.asDictionary())
+    }
+}
